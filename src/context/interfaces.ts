db26085f5b51c95cae6f861e3da3b757924e3fd8
Gabriel Alencar <gabrielalencar33564@gmail.com>
2022-11-11 17:37:10 -0300

    feat: person list

--- src/context/interfaces.ts
+++ src/context/interfaces.ts
@@ -4,6 +4,32 @@ export interface IProvider {
     children: ReactNode;
 }
 
+export interface InfoProps {
+    count: number;
+    pages: number;
+    next: string;
+    prev: null | string;
+}
+
+export interface IResults {
+    id: number | string;
+    name: string;
+    status: string;
+    species: string;
+    gender: string;
+    origin: {
+        name: string;
+    }
+    image: string;
+}
+
+export interface DataProps {
+    data: {
+        info: InfoProps;
+        results: IResults[];
+    }
+}
+
 export interface IContext {
-    
+    personList: IResults[];
 }
